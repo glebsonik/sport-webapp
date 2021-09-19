6af9771505ab0d77c8e6a3ac881a0607927002c5
glebsonik <leeroyglebchik@gmail.com>
2021-09-19 18:35:38 +0300

    Add Sesssions controller with Sign In layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   get 'home/index'
-  # resources :users
 
   root to: "home#index"
   get 'sign_up', to: 'users#new', as: :sign_up
   post 'sign_up', to: 'users#create'
 
   get 'sign_in', to: 'sessions#new', as: :sign_in
+  post 'sign_in', to: 'sessions#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
