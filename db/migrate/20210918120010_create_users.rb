2d8594febc38d6e8a8e7c83c190d4a52d1a64a3c
glebsonik <leeroyglebchik@gmail.com>
2021-09-19 18:22:47 +0300

    Implement Sign Up functionality

---
+++ db/migrate/20210918120010_create_users.rb
@@ -0,0 +1,19 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :email
+      t.string :user_name
+      t.string :password_digest
+      t.string :role
+      t.string :status
+      t.string :first_name
+      t.string :last_name
+      t.string :profile_picture
+      t.date :registered_date
+
+      t.timestamps
+    end
+    add_index :users, :email, unique: true
+    add_index :users, :user_name, unique: true
+  end
+end
