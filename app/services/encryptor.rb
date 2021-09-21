a84874228cb088bc845450d7b561e9824bf3297f
glebsonik <leeroyglebchik@gmail.com>
2021-09-21 21:18:26 +0300

    Refactor Encryptor service obj

--- app/services/encryptor.rb
+++ app/services/encryptor.rb
@@ -1,17 +1,15 @@
-# module Service
-  class Encryptor
+class Encryptor
 
   def initialize(key = Rails.application.credentials.secret_token_key)
     @encryptor = ActiveSupport::MessageEncryptor.new(key)
   end
 
-  def encrypt_data(data)
+  def encrypt(data)
     @encryptor.encrypt_and_sign(data.to_s)
   end
 
-  def decrypt_data(encrypted_data)
+  def decrypt(encrypted_data)
     @encryptor.decrypt_and_verify(encrypted_data)
   end
 
-  end
-# end
+end
