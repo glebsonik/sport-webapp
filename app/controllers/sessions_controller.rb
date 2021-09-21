be9fa364b4f3debb8df2dfdf6a05905a57acbc53
glebsonik <leeroyglebchik@gmail.com>
2021-09-21 21:14:02 +0300

    Refactor session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,18 +4,27 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(user_name: params[:user_name])
+
     if user && user.authenticate(params[:password])
-      reset_session
-      session[:user_token] = Encryptor.new.encrypt_data(user.id)
+      create_user_session!(user)
       redirect_to root_url, notice: "User was successfully created!"
     else
-      flash.now.alert = "Incorrect email or password, try again."
-      render :new
+      redirect_to sign_in_url, alert: "Incorrect email or password, try again."
     end
   end
 
   def destroy
-    session[:user_token] = nil
+    delete_user_session!
     redirect_to root_url, notice: "Logged out"
+  end
+
+  private
+
+  def create_user_session!(user)
+    session[:user_token] = Encryptor.new.encrypt(user.id)
+  end
+
+  def delete_user_session!
+    session[:user_token] = nil
   end
 end
