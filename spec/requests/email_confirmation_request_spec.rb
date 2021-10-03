4b1410d939d9cc799b4ce9de622da119987184ce
glebsonik <leeroyglebchik@gmail.com>
2021-10-03 13:44:23 +0300

    Update existing auto created tests

--- spec/requests/email_confirmation_request_spec.rb
+++ spec/requests/email_confirmation_request_spec.rb
@@ -2,10 +2,16 @@ require 'rails_helper'
 
 RSpec.describe "EmailConfirmations", type: :request do
 
-  describe "GET /index" do
-    it "returns http success" do
-      get "/email_confirmation/index"
-      expect(response).to have_http_status(:success)
+  describe "GET /show" do
+    it "returns redirects if no payload" do
+      get "/email_confirmation/show"
+      expect(response).to have_http_status(:redirect)
+    end
+
+    it "returns redirects if invalid payload" do
+      invalid_payload = {payload: "invalid_payload_value"}
+      get "/email_confirmation/show?#{invalid_payload.to_query}"
+      expect(response).to have_http_status(:redirect)
     end
   end
 
--- spec/views/test/index.html.haml_spec.rb
+++ spec/views/test/index.html.haml_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe "test/index.html.haml", type: :view do
+RSpec.describe "test/show.html.haml", type: :view do
   pending "add some examples to (or delete) #{__FILE__}"
 end
