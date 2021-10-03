7bf52841d1eec46f54dbcbde7d9c2dc99446530c
glebsonik <leeroyglebchik@gmail.com>
2021-10-03 13:41:36 +0300

    Update user mailer service with delimiter in initialize

--- app/services/user_mail_data_processor.rb
+++ app/services/user_mail_data_processor.rb
@@ -1,29 +1,28 @@
 class UserMailDataProcessor
 
-  def initialize
+  def initialize(delimiter = ';;;')
     @encryptor = Encryptor.new(Rails.application.credentials.mail_token_key)
-    @delimiter = ';;;'
+    @delimiter = delimiter
   end
 
   def get_encrypted_payload(user)
     sent_at = Time.zone.now
-    decrypted_payload = "#{user.user_name}#{@delimiter}#{sent_at}"
+    decrypted_payload = "#{user.user_name}#{@delimiter}#{sent_at.to_s}"
     @encryptor.encrypt(decrypted_payload)
   end
 
   def get_user_params(encrypted_payload)
     payload = @encryptor.decrypt(encrypted_payload)
 
-    delimiter_index = payload.rindex(@delimiter)
-    user_name = payload[0..delimiter_index-1]
-    created_at = payload[delimiter_index+1..-1]
+    user_name, created_at = payload.split(@delimiter)
+
     {user_name: user_name, created_at: created_at}
   end
 
   def payload_valid?(encrypted_payload)
     begin
       payload = @encryptor.decrypt(encrypted_payload)
-      payload_valid = payload.rindex(@delimiter).present?
+      payload_valid = payload.index(@delimiter).present?
     rescue Exception
       payload_valid = false
     end
