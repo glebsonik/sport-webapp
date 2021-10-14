55a6d184fd7b05b4626118d394bf9511b976b888
glebsonik <leeroyglebchik@gmail.com>
2021-10-14 14:20:04 +0300

    Fix User model for correct update with no password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
 
   validates :email, :user_name, presence: true, uniqueness: true
   validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
-  validates :password, length: { in: 6..20 }
+  validates :password, length: { in: 6..20 }, allow_nil: true
 
   def active?
     status == 'active'
