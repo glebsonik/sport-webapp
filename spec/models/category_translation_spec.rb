93896c9688978f2bd3188b93ecaf79a7b9c9003e
glebsonik <leeroyglebchik@gmail.com>
2021-10-25 17:55:26 +0300

    Fix readability

--- spec/models/category_translation_spec.rb
+++ spec/models/category_translation_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe CategoryTranslation, type: :model do
   describe 'validations' do
-    it {should belong_to(:category)}
-    it {should belong_to(:language)}
-    it {should validate_presence_of(:name)}
+    it { should belong_to(:category) }
+    it { should belong_to(:language) }
+    it { should validate_presence_of(:name) }
     it { should delegate_method(:key_name).to(:category) }
   end
 
