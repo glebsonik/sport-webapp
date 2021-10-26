fef438ef4b4d50cf9572e3c8f470c9945ab83834
glebsonik <leeroyglebchik@gmail.com>
2021-10-26 19:30:02 +0300

    Add negative API Teams tests

--- spec/requests/api/v1/teams_request_spec.rb
+++ spec/requests/api/v1/teams_request_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe Api::V1::TeamsController, type: :request do
 
   describe "GET /teams" do
-    subject(:send_request) { get "/api/v1/teams/teams?conference_id=#{conference_id}" }
     let(:language_key) { 'en' }
     let(:language) {Language.find_by(key: language_key)}
     let(:conference_key_name) { 'nba_conf_1' }
@@ -17,13 +16,15 @@ RSpec.describe Api::V1::TeamsController, type: :request do
       conference = category.conferences.create!(key_name: conference_key_name)
       conference.conference_translations.create!(language_id: language.id, name: "NBA Conf 1")
       conference.teams.create!(name: team_name)
-
-      send_request
     end
 
     context "when given conference_id param" do
       subject(:parsed_response) {JSON.parse(response.body)}
       let(:expected_team_json) { Team.find_by(name: team_name).as_json(except: [:created_at, :updated_at]) }
+
+      before do
+        get "/api/v1/teams/teams?conference_id=#{conference_id}"
+      end
 
       it "returns http success status when given conference_id param" do
         expect(response).to have_http_status(:success)
@@ -37,6 +38,30 @@ RSpec.describe Api::V1::TeamsController, type: :request do
         expect(parsed_response[0]).to eq(expected_team_json)
       end
     end
+
+    context "when no conference_id given" do
+      before do
+        get "/api/v1/teams/teams"
+      end
+
+      it "returns incorrect status" do
+        expect(response).to have_http_status(400)
+      end
+    end
+
+    context "when invalid conference_id given" do
+      subject(:parsed_response) {JSON.parse(response.body)}
+      let(:invalid_conference_id) {"invalid_id"}
+
+      before do
+        get "/api/v1/teams/teams?conference_id=#{invalid_conference_id}"
+      end
+
+      it "returns empty JSON" do
+        expect(parsed_response).to eq([])
+      end
+    end
+
   end
 
 end
