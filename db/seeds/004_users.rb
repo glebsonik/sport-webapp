c88d6dc36ec77d56230829f3b22d4cce2169fed0
glebsonik <leeroyglebchik@gmail.com>
2021-10-27 22:34:50 +0300

    Optimize user seeds

--- db/seeds/004_users.rb
+++ db/seeds/004_users.rb
@@ -1,9 +1,7 @@
-unless User.find_by(email: 'admin@gmail.com', user_name: 'sadmin')
-  User.create!(email: 'admin@gmail.com',
-               user_name: 'sadmin',
-               password: 'asdf12',
-               role: 'admin',
-               status: 'active',
-               first_name: 'Glebchik',
-               last_name: 'Parkhomenchik')
-end
+User.find_or_create_by!(email: 'admin@gmail.com',
+                        user_name: 'sadmin',
+                        password: 'asdf12',
+                        role: 'admin',
+                        status: 'active',
+                        first_name: 'Glebchik',
+                        last_name: 'Parkhomenchik')
