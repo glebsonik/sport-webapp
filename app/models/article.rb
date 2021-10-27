edcf5cf9f37a2fcfb2ea8eccd06882381b222e6b
glebsonik <leeroyglebchik@gmail.com>
2021-10-27 22:33:19 +0300

    Replace User string with User.name

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
-  belongs_to :author, class_name: "User", foreign_key: :author_id
+  belongs_to :author, class_name: User.name, foreign_key: :author_id
   belongs_to :category
   belongs_to :conference
   belongs_to :team, optional: true
