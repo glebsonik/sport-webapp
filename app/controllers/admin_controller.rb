2d3d56c89fdc8990d2f4116ef197baa702d07370
glebsonik <leeroyglebchik@gmail.com>
2021-11-04 22:55:14 +0200

    Rename action

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,11 +1,11 @@
 class AdminController < ApplicationController
-  before_action :admin_authorize!, :get_categories_layout
+  before_action :admin_authorize!, :categories
 
   layout "admin"
 
   private
 
-  def get_categories_layout
+  def categories
     @translated_categories = CategoryTranslation.where(language_id: current_language.id).includes(:category)
   end
 
