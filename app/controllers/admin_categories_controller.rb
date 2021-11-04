a8d60e068679ea4ff9e69afa6ca82e1c9b917074
glebsonik <leeroyglebchik@gmail.com>
2021-11-04 22:56:11 +0200

    Optimize method invocation in admin categories

--- app/controllers/admin_categories_controller.rb
+++ app/controllers/admin_categories_controller.rb
@@ -1,18 +1,17 @@
 class AdminCategoriesController < AdminController
-
   def show
     @translated_category = CategoryTranslation.translation_for(params[:key_name], current_language_key)
 
     return redirect_to(admin_home_url, alert: 'incorrect category') unless @translated_category
-    prepare_translated_articles
+
+    @translated_articles = prepare_translated_articles(@translated_category.category_id)
   end
 
   private
 
-  def prepare_translated_articles
-    @translated_articles = ArticleTranslation.left_joins(:language, :article)
-                                             .where(languages: { key: current_language_key },
-                                                    articles: { category_id: @translated_category.category_id })
+  def prepare_translated_articles(category_id)
+    ArticleTranslation.left_joins(:language, :article)
+                      .where(languages: { key: current_language_key },
+                             articles: { category_id: category_id })
   end
-
 end
