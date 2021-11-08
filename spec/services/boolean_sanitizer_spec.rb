7e32e6bd7021ef157c37df4f15235c9a84836ea6
glebsonik <leeroyglebchik@gmail.com>
2021-11-08 17:33:53 +0200

    Update specs for Builder and Sanitizer

---
+++ spec/services/boolean_sanitizer_spec.rb
@@ -0,0 +1,20 @@
+
+RSpec.describe BooleanSanitizer, type: :service do
+  describe '.sanitize' do
+    context 'when given boolean truthy string value' do
+      subject(:sanitize) {BooleanSanitizer.sanitize(truthy_string_value)}
+      let(:truthy_string_value) { "1" }
+      it 'returns true' do
+        expect(sanitize).to eq(true)
+      end
+    end
+
+    context 'when given boolean falsy string value' do
+      subject(:sanitize) {BooleanSanitizer.sanitize(falsy_string_value)}
+      let(:falsy_string_value) { "0" }
+      it 'returns false' do
+        expect(sanitize).to eq(false)
+      end
+    end
+  end
+end
