a749ff44fd33e3b52ed9900ae827f1c338a2e4ac
glebsonik <leeroyglebchik@gmail.com>
2021-11-12 13:33:49 +0200

    Add appropriate name for internal API action

--- app/controllers/api/v1/teams_controller.rb
+++ app/controllers/api/v1/teams_controller.rb
@@ -1,7 +1,7 @@
 module Api
   module V1
     class TeamsController < ApplicationController
-      def teams
+      def index
         @teams = params[:conference_id] ? Team.where(conference_id: params[:conference_id]) : Team.all
 
         render json: @teams, except: [:created_at, :updated_at]
