35e74f9ff39e47689e7f3cd596de956028ad8dec
glebsonik <leeroyglebchik@gmail.com>
2021-11-12 16:30:49 +0200

    Update modals on create article page

---
+++ app/assets/javascripts/create_article/cancel_article.js
@@ -0,0 +1,33 @@
+function confirmModalEvent() {
+    console.log(document.getElementById('cancel-article-btn').dataValue);
+    location.href = document.getElementById('cancel-article-btn').getAttribute('data-value');
+
+}
+
+function showModal(modal) {
+    modal.style.display = 'block';
+}
+
+function hideModal(modal) {
+    modal.style.display = 'none';
+}
+
+window.addEventListener('load', () => {
+    const modal = document.getElementById('cancel-article-modal');
+    const modalOuterSpace = document.getElementById('cancel-modal-outer');
+    modalOuterSpace.addEventListener('click', (e) => {
+        if (e.target === modalOuterSpace)
+            hideModal(modal);
+    });
+
+    document.getElementById('cancel-article-btn').addEventListener('click', (e) => {
+        showModal(modal);
+    });
+
+    document.getElementById('cancel-modal-confirm').addEventListener('click', (e)=> {
+        confirmModalEvent();
+    });
+
+    document.getElementById('cancel-modal-cancel').addEventListener('click', (e) => {
+        hideModal(modal)});
+});
