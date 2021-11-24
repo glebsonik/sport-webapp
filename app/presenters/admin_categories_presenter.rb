47349314269bf85224f3d846878ea90985ba13c4
glebsonik <leeroyglebchik@gmail.com>
2021-11-24 16:41:30 +0200

    Refactor namings

--- app/presenters/admin_categories_presenter.rb
+++ app/presenters/admin_categories_presenter.rb
@@ -12,22 +12,18 @@ class AdminCategoriesPresenter
 
   def articles
     @articles ||= ArticleTranslation.left_joins(:language, :article)
-                        .where(*where_params)
-                        .left_joins(*article_join_params)
-                        .select(select_string)
+                        .where(*article_translation_query)
+                        .left_joins(:article, { article: :team }, article: {category: :category_translations})
+                        .select(selected_fields)
   end
 
   private
 
-  def where_params
+  def article_translation_query
     [languages: { key: @language_key }, articles: { category_id: @category_translation.category_id }]
   end
 
-  def article_join_params
-    [:article, { article: :team }, article: {category: :category_translations}]
-  end
-
-  def select_string
+  def selected_fields
     'article_translations.*, category_translations.name as category_name, teams.name as team_name, articles.id as article_id'
   end
 
