bd2b4fa6deec941c5397cec965a51d385411fac0
glebsonik <leeroyglebchik@gmail.com>
2021-12-07 22:15:11 +0200

    Refactor specs for navigation builder

--- spec/services/user_navigation_builder_spec.rb
+++ spec/services/user_navigation_builder_spec.rb
@@ -1,9 +1,5 @@
 require './app/services/builders/user_navigation_builder.rb'
 
-#TODO: discuss is that enough for testing or how can we expand this example
-# My opinion that it should be expanded by increasing conferences to 4 (2 for each category)
-# Usage of a helper method?
-
 RSpec.describe UserNavigationBuilder, type: :service do
   describe "#build" do
     subject(:build) {UserNavigationBuilder.new(language).build}
@@ -11,13 +7,16 @@ RSpec.describe UserNavigationBuilder, type: :service do
     let(:conferences) do
       [
       create(:conference, :with_translation, category: categories[0], language: language),
-      create(:conference, :with_translation, category: categories[1], language: language)
+      create(:conference, :with_translation, category: categories[0], language: language),
+      create(:conference, :with_translation, category: categories[1], language: language),
       ]
     end
     let(:teams) do
       [
         create(:team, conference: conferences[0]),
-        create(:team, conference: conferences[1])
+        create(:team, conference: conferences[0]),
+        create(:team, conference: conferences[1]),
+        create(:team, conference: conferences[2])
       ]
     end
     let(:language) { create(:language) }
@@ -32,7 +31,17 @@ RSpec.describe UserNavigationBuilder, type: :service do
               name: conferences[0].conference_translations[0].name,
               teams: [
                 { id: teams[0].id,
-                  name: teams[0].name }
+                  name: teams[0].name },
+                { id: teams[1].id,
+                  name: teams[1].name },
+              ]
+            },
+            {
+              key: conferences[1].key,
+              name: conferences[1].conference_translations[0].name,
+              teams: [
+                { id: teams[2].id,
+                  name: teams[2].name }
               ]
             }
           ] },
@@ -40,14 +49,14 @@ RSpec.describe UserNavigationBuilder, type: :service do
           name: categories[1].category_translations[0].name,
           conferences: [
             {
-              key: conferences[1].key,
-              name: conferences[1].conference_translations[0].name,
+              key: conferences[2].key,
+              name: conferences[2].conference_translations[0].name,
               teams: [
-                { id: teams[1].id,
-                  name: teams[1].name }
+                { id: teams[3].id,
+                  name: teams[3].name }
               ]
             }
-          ] }
+          ] },
       ]}
     end
 
@@ -58,7 +67,7 @@ RSpec.describe UserNavigationBuilder, type: :service do
       teams
     end
 
-    it "returns expected navigation structure" do
+    it "returns expected navigation structure without unfinished navigation" do
       expect(build).to eq(expected)
     end
   end
