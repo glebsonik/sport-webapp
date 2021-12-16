4f5321b089a36c0c05b9b9a5925b154dc5c06e67
glebsonik <leeroyglebchik@gmail.com>
2021-12-16 23:14:04 +0200

    Update rails autoload path

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,9 @@ module SportWebapp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    Dir[Rails.root.join('app', 'services', '*')].each do |dir|
+      config.autoload_paths << dir
+    end
     config.generators do |g|
       g.test_framework :rspec
       g.template_engine :haml
